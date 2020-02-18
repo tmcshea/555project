180f08c6b4bca84d4ef9a1a5cdb9691093f845e0
Aaron Jin <ajxks35@gmail.com>
2020-02-17 20:28:17 -0500

    week4 Aaron unittests

---
+++ test_datesBeforeCurrentDate.py
@@ -0,0 +1,19 @@
+import unittest
+import parser
+
+class TestCase(unittest.TestCase):
+	# def setUp(self):
+	# 	return
+
+	# def tearDown(self):
+	# 	self.file.close()
+
+	def test_A(self):
+		return
+
+	def test_B(self):
+		return
+	
+
+if __name__ == "__main__":
+	unittest.main()
